dd08d731fd144ac663871415f7b8e9bfbdd38d2d
yuma <kitamuramint@gmail.com>
2020-06-20 13:45:34 +0900

    2020/06/20 First Commit

---
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -0,0 +1,10 @@
+package com.example.demo.app.inquiry;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class InquiryController {
+
+}
